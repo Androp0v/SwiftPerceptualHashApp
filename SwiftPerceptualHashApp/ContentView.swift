df653377cb630dbbb7f622bee1b9567c5046f9b0
Raúl Montón Pinillos <raul_m_p@me.com>
2023-04-12 23:22:06 +0200

    Initial Commit

---
+++ SwiftPerceptualHashApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftPerceptualHashApp
+//
+//  Created by Raúl Montón Pinillos on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPerceptualHashApp/SwiftPerceptualHashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPerceptualHashAppApp.swift
+//  SwiftPerceptualHashApp
+//
+//  Created by Raúl Montón Pinillos on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPerceptualHashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
